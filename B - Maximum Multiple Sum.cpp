62b4c847fe1d7ecaf67a891b3ebeb4c77d33170f
Golam Rabbani <164948183+GOLAM4RABBANI@users.noreply.github.com>
2024-06-12 05:58:32 +0600

    Create B - Maximum Multiple Sum.cpp

---
+++ B - Maximum Multiple Sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  optimize() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define pb push_back
+
+
+int main(){
+    optimize();
+    int n;
+    cin >> n;
+    while(n--){
+        
+        int k;
+        cin >> k;
+        if(k==3){
+         cout << 3 << endl;   
+        }else{
+            cout << 2 << endl;
+        }
+        }
+        return 0;
+    }
+    
+    
+    
+    
